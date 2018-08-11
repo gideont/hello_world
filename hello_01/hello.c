7e55d7b01470807de9011d35a7e4d19b41747216
Gideon Tsang <gideont@gmail.com>
2018-08-12 00:34:19 +0800

    Adding main program arguments to hello_02 and added a simple copyright banner

--- hello_01/hello.c
+++ hello_01/hello.c
@@ -1,5 +1,11 @@
+/***********************************************
+*      Copyright (C) Gideon Tsang, 2018        *
+*                                              *
+* The most basic hello world sample code in C. *
+***********************************************/
+
 #include <stdio.h>
- 
+
 int main(void)
 {
     printf("Hello world!\n");
--- hello_02_shared_lib/src/hello.c
+++ hello_02_shared_lib/src/hello.c
@@ -1,9 +1,15 @@
+/***********************************************
+*      Copyright (C) Gideon Tsang, 2018        *
+*                                              *
+***********************************************/
+
 #include <stdio.h>
 #include "foo.h"
  
-int main(void)
+int main(int argc, char *argv[])
 {
     printf("Hello world!\n");
     foo();
+
     return 0;
 }
